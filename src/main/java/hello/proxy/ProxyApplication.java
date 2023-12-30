7718934792f1e90854940bb653d0b95d3e9c7e65
Minjae Kim <wnsqud7070@gmail.com>
2023-12-30 19:30:03 +0900

    :white_check_mark: 구체클래스

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,14 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
-@Import(AppV1Config.class) // 빈 수동등록 설정
+//@Import(AppV1Config.class) // 빈 수동등록 설정
+@Import(AppV2Config.class)
 public class ProxyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hello/proxy/app/V2/OrderRepositoryV2.java
@@ -0,0 +1,20 @@
+package hello.proxy.app.V2;
+
+public class OrderRepositoryV2 {
+
+    public void save(String itemId) {
+        if(itemId.equals("ex)")){
+            throw new IllegalArgumentException("예외발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int mills) {
+
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
